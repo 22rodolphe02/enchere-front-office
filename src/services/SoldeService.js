cbcb2a4ad1cbd6bb22250d56b857ad4b12594f11
22rodolphe02 <98727714+22rodolphe02@users.noreply.github.com>
2023-02-03 20:42:31 +0300

    Update SoldeService.js

--- src/services/SoldeService.js
+++ src/services/SoldeService.js
@@ -1,9 +1,9 @@
 import {api} from "../utils/api";
 
 export async function listRequestNonValid(){
-    return (await api.get("/api/soldes/depot-non-valid")).data
+    return (await api.get("/api/projetEnchere/soldes/depot-non-valid")).data
 }
 
 export async function updateNonValid(data){
-    return (await api.post("/api/soldes/update", data)).data
+    return (await api.post("/api/projetEnchere/soldes/update", data)).data
 }
